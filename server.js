9636bd48126f02662be8ebdb2d57d840c4ea2a15
mridul199105 <30659081+mridul199105@users.noreply.github.com>
2017-08-05 20:33:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,8 +103,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:archiveme', function (req, res) {
-  res.send(createtemplate(datastuff.archiveme));
+app.get('/archiveone', function (req, res) {
+  res.send(createtemplate(datastuff.archiveone));
 });
 
 app.get('/ui/style.css', function (req, res) {
