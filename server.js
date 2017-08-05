1c7aa1b883093296b86c37a98ed19bf2b7b1738f
mridul199105 <30659081+mridul199105@users.noreply.github.com>
2017-08-05 14:16:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-function opennav() {
+var opennav = function() {
     document.getElementById("mysidenav").style.width = "250px";
     document.getElementById("main").style.marginleft = "250px";
 }
-function closenav() {
+var closenav = function() {
     document.getElementById("mysidenav").style.width = "0";
     document.getElementById("main").style.marginleft = "0";
 }
