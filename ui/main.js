94a3d71bec2ff9071a82e53ee4af02155a81034d
mridul199105 <30659081+mridul199105@users.noreply.github.com>
2017-08-06 12:55:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,13 @@
 console.log('Loaded!');
 
 function opennav() {
+            document.getElementById("main").style.transition = "0.5s";
             document.getElementById("mysidenav").style.width = "250px";
             document.getElementById("main").style.marginLeft = "250px";
             document.getElementById("main").style.maxWidth = "720px";
         }
         function closenav() {
+            document.getElementById("main").style.transition = "0.5s";
             document.getElementById("mysidenav").style.width = "0";
             document.getElementById("main").style.marginLeft = "0";
             document.getElementById("main").style.maxWidth = "970px";
