de32687bf10e38e30a121eed2c1ccd8d60795861
mridul199105 <30659081+mridul199105@users.noreply.github.com>
2017-08-06 16:52:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/ui/main.js', function (req, res) {
-   res.sendfile(path.join(__dirname, 'ui', 'main.js')); 
+   res.sendFile(path.join(__dirname, 'ui', 'main.js')); 
 });
 
 
