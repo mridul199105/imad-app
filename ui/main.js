6ef0a3224f218021242757601e81ad8a3253a0cf
mridul199105 <30659081+mridul199105@users.noreply.github.com>
2017-08-06 12:52:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,13 @@ function opennav() {
             document.getElementById("mysidenav").style.width = "250px";
             document.getElementById("main").style.marginLeft = "250px";
             document.getElementById("main").style.maxWidth = "720px";
+            document.getElementById("main").style.transition = "0.5s";
         }
         function closenav() {
             document.getElementById("mysidenav").style.width = "0";
             document.getElementById("main").style.marginLeft = "0";
             document.getElementById("main").style.maxWidth = "970px";
+            document.getElementById("main").style.transition = "0.5s";
         }
     
     var i = 0;
