3f0ac006fcede8363f553221e262151d974b7388
mridul199105 <30659081+mridul199105@users.noreply.github.com>
2017-08-06 12:51:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ function opennav() {
         function closenav() {
             document.getElementById("mysidenav").style.width = "0";
             document.getElementById("main").style.marginLeft = "0";
-            document.getElementById("main").style.maxWidth = "9700px";
+            document.getElementById("main").style.maxWidth = "970px";
         }
     
     var i = 0;
