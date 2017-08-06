413e51249b0b0293ceb67090c728b2a5aab0b617
mridul199105 <30659081+mridul199105@users.noreply.github.com>
2017-08-06 12:44:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,12 @@ console.log('Loaded!');
 
 function opennav() {
             document.getElementById("mysidenav").style.width = "250px";
-            document.getElementById("main").style.marginleft = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
         }
         function closenav() {
             document.getElementById("mysidenav").style.width = "0";
-            document.getElementById("main").style.marginleft = "0";
+            document.getElementById("main").style.marginLeft = "0";
         }
     
-    alert("who goes there...");
+    var i = 0;
+    
