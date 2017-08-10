2cc80f5509f18ea8651b1bc9062d65205e42f4a8
mridul199105 <30659081+mridul199105@users.noreply.github.com>
2017-08-10 17:46:01 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,1 +1,33 @@
+console.log('Loaded!');
 
+function opennavind() {
+            document.getElementById("bg").style.transition = "0.5s";
+            document.getElementById("mysidenav").style.width = "250px";
+            document.getElementById("bg").style.marginLeft = "-235px";
+            document.getElementById("bg").style.maxWidth = "720px";
+}
+        
+function closenavind() {
+            document.getElementById("bg").style.transition = "0.5s";
+            document.getElementById("mysidenav").style.width = "0";
+            document.getElementById("bg").style.marginLeft = "-485px";
+            document.getElementById("bg").style.maxWidth = "970px";
+}
+
+var button = document.getElementById("buon");
+button.onClick = function() {
+    alert("it works");
+    var repl = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if ( request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var n = request.responseText;
+                var cont = document.getElementById("cntMe");
+                cont.innerHTML = n.toString();
+            }
+        }
+    };
+    request.open('GET', 'http://mridul199105.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
